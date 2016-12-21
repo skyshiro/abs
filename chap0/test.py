5a7ccc30d0a3c5d6a86e1b07f28c45ac78c98225
Woof <hodge.tannis@gmail.com>
2016-12-20 21:56:17 -0800

    This is actually chapter 1. off to a good start!

--- chap0/test.py
+++ chap0/test.py
@@ -4,3 +4,41 @@ Type "copyright", "credits" or "license()" for more information.
 2+2
 4
 >>> 
+2**4
+16
+>>> 
+======== RESTART: C:/Users/Tannis/Documents/GitHub/abs/chap0/chap0.py ========
+Hello World
+Name?
+Tannis
+HelloTannis
+Traceback (most recent call last):
+  File "C:/Users/Tannis/Documents/GitHub/abs/chap0/chap0.py", line 5, in <module>
+    print('The length of your name is: ' + len(myName))
+TypeError: Can't convert 'int' object to str implicitly
+>>> 
+======== RESTART: C:/Users/Tannis/Documents/GitHub/abs/chap0/chap0.py ========
+Hello World
+Name?
+Tannis
+HelloTannis
+Traceback (most recent call last):
+  File "C:/Users/Tannis/Documents/GitHub/abs/chap0/chap0.py", line 5, in <module>
+    print('The length of your name is: ' + string(len(myName)))
+NameError: name 'string' is not defined
+>>> 
+======== RESTART: C:/Users/Tannis/Documents/GitHub/abs/chap0/chap0.py ========
+Hello World
+Name?
+Tannis
+HelloTannis
+The length of your name is: 6
+>>> 
+======== RESTART: C:/Users/Tannis/Documents/GitHub/abs/chap0/chap0.py ========
+Hello World
+Name?
+Tannis
+Hello Tannis
+The length of your name is:
+6
+>>> 
